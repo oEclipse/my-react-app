bc144c9927ca7e315e9660c8cc55780d72d74888
oEclipse <ekals0070@naver.com>
2024-06-26 17:29:47 +0900

    ##2 패키지 버전 및 개발 환경(code linter), 편의 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'components/HeaderContent'
+import HeaderContent from "components/HeaderContent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderContent />
     </div>
   );
 }
